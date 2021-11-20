567dfb0ecce32e70072f946ccfe253528544cf7d
Marvin Buff <marvinbuff@hotmail.com>
2021-11-20 19:35:02 +0100

    add mock player ui

--- game/Player.gd
+++ game/Player.gd
@@ -1,7 +1,20 @@
 extends KinematicBody2D
 
-export var player_id: int = 1
+enum PlayerId {
+	Player1, Player2
+}
+
+export (PlayerId) var player_id
 const SPEED = 15000
+
+export var INITIAL_HEALTH = 100
+var health = INITIAL_HEALTH
+
+func _ready():
+	if (player_id != PlayerId.Player1):
+		remove_child($GUI_Player1)
+	if (player_id != PlayerId.Player2):
+		remove_child($GUI_Player2)
 
 func _physics_process(delta):
 	var velocity = Input.get_vector(
@@ -11,3 +24,6 @@ func _physics_process(delta):
 		str("move_down_player", player_id))
 	
 	move_and_slide(velocity * delta * SPEED)
+
+func hit_by_projectile(projectile):
+	health -= 21.5
