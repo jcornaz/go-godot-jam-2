95bc51492992e3da42cb447b156ab7382404a2d1
Marvin Buff <marvinbuff@hotmail.com>
2021-11-20 19:50:14 +0100

    connecting mock ui

--- game/Player.gd
+++ game/Player.gd
@@ -11,19 +11,37 @@ export var INITIAL_HEALTH = 100
 var health = INITIAL_HEALTH
 
 func _ready():
-	if (player_id != PlayerId.Player1):
-		remove_child($GUI_Player1)
-	if (player_id != PlayerId.Player2):
-		remove_child($GUI_Player2)
+	_hide_other_player_huds()
+	_update_health_indicators()
 
 func _physics_process(delta):
 	var velocity = Input.get_vector(
-		str("move_left_player", player_id), 
-		str("move_right_player", player_id), 
-		str("move_up_player", player_id), 
-		str("move_down_player", player_id))
+		str("move_left_player", player_id + 1), 
+		str("move_right_player", player_id + 1), 
+		str("move_up_player", player_id + 1), 
+		str("move_down_player", player_id + 1))
+	
+	if (Input.is_action_just_pressed("ui_accept")):
+		hit_by_projectile(null)
 	
 	move_and_slide(velocity * delta * SPEED)
 
 func hit_by_projectile(projectile):
-	health -= 21.5
+	health -= 20.0
+	_update_health_indicators()
+
+func _update_health_indicators():
+	if (player_id == PlayerId.Player1):
+		_update_health_indicator($HUD/HUD_Player1/ProgressBar)
+	if (player_id == PlayerId.Player2):
+		_update_health_indicator($HUD/HUD_Player2/ProgressBar)
+
+func _update_health_indicator(health_indicator: ProgressBar):
+	health_indicator.value = health
+	health_indicator.max_value = INITIAL_HEALTH
+
+func _hide_other_player_huds():
+	if (player_id != PlayerId.Player1):
+		$HUD/HUD_Player1.visible = false
+	if (player_id != PlayerId.Player2):
+		$HUD/HUD_Player2.visible = false
