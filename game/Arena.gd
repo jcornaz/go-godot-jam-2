a8934a7aef0847f947eca3a434f88f4351b4f793
klooser <kevin.looser@optravis.com>
2021-11-26 16:06:27 +0100

    add main menu scene to arena

--- game/Arena.gd
+++ game/Arena.gd
@@ -4,4 +4,3 @@ extends Node2D
 func _ready():
 	$HUD.set_player_max_health(1, $Players/Player1.INITIAL_HEALTH)
 	$HUD.set_player_max_health(2, $Players/Player2.INITIAL_HEALTH)
-
--- game/gui/MainMenu.gd
+++ game/gui/MainMenu.gd
@@ -15,9 +15,14 @@ func _on_StartGame_pressed():
 func _on_EndGame_pressed():
 	get_tree().quit()
 
-func handle_player_death(player_id):
-	print("TODO: call this function on emitted player death signal")
-	$Menu/View/EndGameMessage.text = "Player " + player_id + " won!"
+
+func handle_player_death(player_id: String):
+	$Menu/View/EndGameMessage.text = "Player " + player_id + " perished!"
 	$Menu/View/ItemList/StartGame.text = "Play Again"
 	$Menu.show()
 	get_tree().paused = true
+
+
+func _on_Player_HealthChanged(health: float, player_num: int):
+	if (health <= 0.0):
+		handle_player_death(str(player_num))
