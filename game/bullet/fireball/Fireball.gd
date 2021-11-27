9c5e5463a7eba0df78c3ee083db62753ce7fc899
Marvin Buff <marvinbuff@hotmail.com>
2021-11-27 10:16:43 +0100

    add fireball mock as a copy of spark

---
+++ game/bullet/fireball/Fireball.gd
@@ -0,0 +1,14 @@
+extends RigidBody2D
+
+
+func initialize(velocity: Vector2):
+	apply_impulse(Vector2.ZERO, velocity * mass)
+
+
+func _on_Hitbox_entered(area):
+	area.deal_damage(100)
+	queue_free()
+
+
+func _on_body_entered(body):
+	queue_free()
