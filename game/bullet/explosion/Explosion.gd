d27dbf7248cc8f2386774c2650619b3588b7a9cb
Marvin Buff <marvinbuff@hotmail.com>
2021-11-27 15:42:16 +0100

    add fireball prototype

---
+++ game/bullet/explosion/Explosion.gd
@@ -0,0 +1,21 @@
+extends AnimatedSprite
+
+signal explosion_peak
+
+const PEAK_FRAME = 2
+const BASE_RADIUS  = 17.5
+
+func initialize(node: Node2D, radius: float):
+	var scale = radius/BASE_RADIUS
+	self.scale = Vector2(scale, scale)
+	self.global_position = node.global_position
+	connect("explosion_peak", node, "_on_explode_peak")
+	play()
+
+
+func _count_to_the_peak():
+	if frame == 2:
+		emit_signal("explosion_peak")
+
+func _explosion_finished():
+	queue_free()
--- game/bullet/fireball/Fireball.gd
+++ game/bullet/fireball/Fireball.gd
@@ -1,20 +1,37 @@
 extends RigidBody2D
 
+export var EXPLOSION: PackedScene
+export var EXPLOSION_RADIUS: float = 100
+
+var exploded = false
+
+func _ready():
+	($ExplosionHitbox/CollisionShape2D.shape as CircleShape2D).radius = EXPLOSION_RADIUS
+
 
 func initialize(velocity: Vector2):
 	apply_impulse(Vector2.ZERO, velocity * mass)
 
 
 func _on_Hitbox_entered(area):
-	area.deal_damage(20)
-	queue_free()
+	_on_explode()
 
 
 func _on_body_entered(body):
-	queue_free()
+	_on_explode()
 
 
 func _on_explode():
+	if exploded == false:
+		exploded = true
+		hide()
+		sleeping = true
+		var explosion = EXPLOSION.instance()
+		get_parent().add_child(explosion)
+		explosion.initialize(self, EXPLOSION_RADIUS)
+	
+
+func _on_explode_peak():
 	var hitPlayerHurtBoxes = $ExplosionHitbox.get_overlapping_areas()
 	for hitPlayerHurtBox in hitPlayerHurtBoxes:
 		hitPlayerHurtBox.deal_damage(100)
