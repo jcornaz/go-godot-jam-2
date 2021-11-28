0622eb5d057ac35bee7a076276e5089c371d7115
klooser <kevin.looser@optravis.com>
2021-11-28 11:16:49 +0100

    improve main menu, add controls description

--- game/gui/MainMenu.gd
+++ game/gui/MainMenu.gd
@@ -5,6 +5,8 @@ signal game_started
 
 func _ready():
 	get_tree().paused = true
+	$Menu/View/ItemList/StartGame.grab_focus()
+
 
 func _on_StartGame_pressed():
 	$Menu.hide()
@@ -19,6 +21,7 @@ func _on_EndGame_pressed():
 func handle_player_death(player_id: String):
 	$Menu/View/EndGameMessage.text = "Player " + player_id + " perished!"
 	$Menu/View/ItemList/StartGame.text = "Play Again"
+	$Menu/View/ItemList/StartGame.grab_focus()
 	$Menu.show()
 	get_tree().paused = true
 
@@ -26,3 +29,30 @@ func handle_player_death(player_id: String):
 func _on_Player_HealthChanged(health: float, player_num: int):
 	if (health <= 0.0):
 		handle_player_death(str(player_num))
+		get_tree().call_group("Bullet", "queue_free")
+
+
+func show_controls():
+	$Menu/ControlsPanel.show()
+	$Menu/View/ItemList/StartGame.disabled = true
+	$Menu/View/ItemList/ShowControls.disabled = true
+	$Menu/View/ItemList/EndGame.disabled = true
+	$Menu/ControlsPanel/ControlsView/Back.disabled = false
+	$Menu/ControlsPanel/ControlsView/Back.grab_focus()
+
+
+func hide_controls():
+	$Menu/ControlsPanel.hide()
+	$Menu/View/ItemList/StartGame.disabled = false
+	$Menu/View/ItemList/ShowControls.disabled = false
+	$Menu/View/ItemList/EndGame.disabled = false
+	$Menu/ControlsPanel/ControlsView/Back.disabled = true
+	$Menu/View/ItemList/StartGame.grab_focus()
+
+
+func _on_ShowControls_pressed():
+	show_controls()
+
+
+func _on_Button_pressed():
+	hide_controls()
