abcc978bdb2aa2d78513f65c8eeec5111c01b672
klooser <kevin.looser@optravis.com>
2021-11-29 21:07:26 +0100

    add event to the arena

--- game/gui/HUD.gd
+++ game/gui/HUD.gd
@@ -15,3 +15,27 @@ func _on_player_EnergySet(slot: int, ability: Ability, player_num: int):
 	var texture: TextureRect = get_node(str("Player_", player_num, "/Slot", slot))
 	texture.texture = ability.texture
 	texture.visible = true
+
+
+func set_event_max_time(max_time: float):
+	$Event/ProgressBar.max_value = max_time
+
+
+func _on_Event_event_ended():
+	$Event.visible = false
+	$Event/ProgressBar.value = $Event/ProgressBar.max_value
+
+
+func _on_Event_event_started():
+	$Event.visible = true
+
+
+func _on_Event_count_down(time: float):
+	$Event/ProgressBar.value = time
+
+
+func _on_Event_player_scores(player_scores: Dictionary):
+	if (player_scores.has(0)):
+		$Event/Player_1_Score.text = str(round(player_scores.get(0)))
+	if (player_scores.has(1)):
+		$Event/Player_2_Score.text = str(round(player_scores.get(1)))
