ca6a9557128fe730b54d9610e3ba073ff04a03fc
Marvin Buff <marvinbuff@hotmail.com>
2021-12-10 10:08:32 +0100

    minor refactoring

--- game/arena/4-players/Splitscreen.gd
+++ game/arena/4-players/Splitscreen.gd
@@ -2,7 +2,7 @@ extends CanvasLayer
 
 export var WORLD: PackedScene
 
-onready var viewport1 = $"MarginContainer/GridContainer/Container 1/Viewport 1"
+onready var viewport1 = $"MarginContainer/GridContainer/Container 1/Viewport"
 onready var grid = $"MarginContainer/GridContainer"
 
 
@@ -11,13 +11,13 @@ func _ready():
 	viewport1.add_child(world, true)
 	
 	for i in range(1,grid.get_child_count() + 1):
-		var view : Viewport = get_node("MarginContainer/GridContainer/Container " + str(i) + "/Viewport " + str(i))
+		var view : Viewport = get_node("MarginContainer/GridContainer/Container " + str(i) + "/Viewport")
 		if(view != null):
 			var zoom_size = 1.5
-			var cam : Camera2D = view.get_node("Camera2D")
+			var cam : Camera2D = view.get_node("Camera")
 			
 			view.world_2d = viewport1.world_2d
 			
-			var player = get_node("MarginContainer/GridContainer/Container 1/Viewport 1/Arena 4 Players/Players/Player" + str(i))
+			var player = get_node("MarginContainer/GridContainer/Container 1/Viewport/Arena 4 Players/Players/Player" + str(i))
 			cam.zoom = Vector2(zoom_size,zoom_size)
 			cam.target = player
