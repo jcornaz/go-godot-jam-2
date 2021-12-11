ed75498495c51c110d4a210560e2dd6fa19f8032
Marvin Buff <marvinbuff@hotmail.com>
2021-12-11 10:32:20 +0100

    connect health bar to player health

--- game/arena/4-players/PlayerViewportContainer.gd
+++ game/arena/4-players/PlayerViewportContainer.gd
@@ -4,7 +4,11 @@ var target: Player = null setget _set_target
 
 func _set_target(value: Player):
 	target = value
-	$"Viewport/PlayerHUD".player_id = target.player_id
+	var hud = $"Viewport/PlayerHUD"
+	hud.player_id = target.player_id
+	hud.set_player_max_health(target.INITIAL_HEALTH)
+	value.connect("HealthChanged", hud, "_on_player_HealthChanged")
+	value.connect("EnergySet", hud, "_on_player_EnergySet")
 
 func initialize(world: World2D, player: Player):
 	$Viewport.world_2d = world
