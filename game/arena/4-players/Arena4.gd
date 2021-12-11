b571416b36336c3fb6f2f0adaba8402f8eee0132
Marvin Buff <marvinbuff@hotmail.com>
2021-12-11 10:41:01 +0100

    connect game over signal

--- game/arena/4-players/Arena4.gd
+++ game/arena/4-players/Arena4.gd
@@ -2,11 +2,6 @@ extends Node2D
 
 signal GameFinished(num)
 
-func _ready():
-	pass
-	#$HUD.set_player_max_health(1, $Players/Player1.INITIAL_HEALTH)
-	#$HUD.set_player_max_health(2, $Players/Player2.INITIAL_HEALTH)
-
 
 func _on_Player_Dead(player_num: int):
 	emit_signal("GameFinished", player_num)
--- game/arena/4-players/Splitscreen.gd
+++ game/arena/4-players/Splitscreen.gd
@@ -5,6 +5,7 @@ export var WORLD: PackedScene
 onready var viewport1 = $"MarginContainer/GridContainer/Container 1/Viewport"
 onready var grid = $"MarginContainer/GridContainer"
 
+signal GameFinished(num)
 
 func _ready():
 	var world = WORLD.instance()
@@ -16,3 +17,9 @@ func _ready():
 			
 			var player = get_node("MarginContainer/GridContainer/Container 1/Viewport/Arena 4 Players/Players/Player" + str(i))
 			container.initialize(viewport1.world_2d, player)
+	
+	world.connect("GameFinished", self, "_propagate_game_finished")
+
+
+func _propagate_game_finished(player_num: int):
+	emit_signal("GameFinished", player_num)
